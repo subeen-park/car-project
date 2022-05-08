7766bd095d7300e05f6078d55e05dcc337d9a7cc
subeen <mas_jaypark@naver.com>
2022-05-08 19:02:10 +0900

    <<10주차>> 카풀 등록 유효성 검사 구현. => 카풀 등록 100% 구현, 추가 소요시간, 탑승자 탑승 시간은 x

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,59 +8,6 @@
 
 
 
-<!-- 날짜 선택 , 달력 --> <!--
-    <v-row>
-    <v-col
-      cols="5"
-    >
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="date"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-combobox
-            v-model="date"
-            multiple
-            chips
-            small-chips
-            label="카풀 선택 날짜"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-combobox>
-        </template>
-        <v-date-picker
-          v-model="date"
-          no-title
-          scrollable
-          multiple
-        >
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="menu = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            text
-            color="primary"
-            @click="$refs.menu.save(date)"
-          >
-            OK
-          </v-btn>
-        </v-date-picker>
-      </v-menu>
-    </v-col>
-    </v-row> <!-- 달력 -->
-
     <v-col
     sm="6">
       <v-text-field
@@ -80,11 +27,12 @@
 
 <br>
 
-      <v-row justify="center"> <!-- 카풀 등록 dialog, 운전자용 -->
+      <v-row > <!-- 카풀 등록 dialog, 운전자용 -->
 
 <v-col
-cols="12"
-sm="6">
+  cols="12"
+  m="6">
+
     <v-dialog
       v-model="dialog"
       max-width="600px"
@@ -98,103 +46,135 @@ sm="6">
           v-on="on"
           
         >
-        
-        
          카풀 등록
         </v-btn>
     
       </template>
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">운전자 정보</span>
-        </v-card-title>
+       <v-card ref="form">
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col
+
+          <v-col
                 cols="12"
                 sm="6"
                 md="4"
               >
-                <v-text-field
-                  label="운전자 이름*"
-                  ref="input_name"
-                  v-model="input_name"
-                  :rules="[() => !!input_name  || '이름을 입력하세요.']"
-                  :error-messages="errorMessages"
-                  required
-                ></v-text-field>
-              </v-col>
+          <v-text-field
+            ref="input_name"
+            v-model="input_name"
+            :rules="[() => !!input_name || '이름을 입력하세요']"
+            :error-messages="errorMessages"
+            label="운전자 이름*"
+            placeholder="ex) 김인하"
+            required
+          ></v-text-field>
+          </v-col>
 
-              <v-col
+
+          <v-col
                 cols="12"
                 sm="6"
                 md="4"
               >
-               <v-select
-                  :items="['여자', '남자']"
-                  label="성별*"
-                  v-model="input_gender"
-                  :rules="[() => !!input_gender || '성별을 선택하세요.']"
-                  required
-                ></v-select>
-              </v-col>
+          <v-select
+            :items="['여자', '남자']"
+            ref="input_gender"
+            v-model="input_gender"
+            :rules="[() => !!input_gender || '성별을 선택하세요']"
+            label="성별*"
+            required
+          ></v-select>
+          </v-col>
 
-              <v-col
+
+
+         <v-col
                 cols="12"
                 sm="6"
                 md="4"
               >
-                <v-select
-                  :items="['1', '2', '3', '4', '5', '6']"
-                  label="탑승 가능 인원*"
-                  v-model="input_seat"
-                  :rules="[() => !!input_seat || '성별을 선택하세요.']"
-                ></v-select>
-              </v-col>
+          <v-select
+            :items="['1', '2', '3', '4', '5', '6']"
+            ref="input_seat"
+            v-model="input_seat"
+            :rules="[() => !!input_seat || '탑승 가능 인원을 선택하세요']"
+            label="탑승 가능 인원*"
+            required
+          ></v-select>
+         </v-col>
 
-              <v-col
+
+
+
+          <v-col
               sm="6">
-                <v-text-field
-                  label="카풀 기간 입력 (ex.일주일)*"
-                  v-model="input_period"
-                  :rules="[() => !!input_period || '기간을 입력하세요.']"
-                ></v-text-field>
-              </v-col>
+          <v-text-field
+            ref="input_period"
+            v-model="input_period"
+            :rules="[() => !!input_period || 'This field is required']"
+            label="카풀 기간 입력*"
+            required
+            placeholder="ex) 1개월"
+          ></v-text-field>
+          </v-col>
 
-              <v-col
+
+
+
+          <v-col
               sm="6">
-                <v-autocomplete
-                  :items="['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']"
-                  label="카풀 요일 선택*"
-                  v-model="input_day"
-                  :rules="[() => !!input_day || '날짜를 선택하세요.']"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
+           <v-autocomplete
+            :items="['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']"
+            ref="input_day"
+            v-model="input_day"
+            :rules="[() => !!input_day || '요일을 선택하세요']"
+            label="카풀 요일 선택*"
+            required
+          >
+           </v-autocomplete>
+          </v-col>
 
 
-              <v-col 
-              cols="12"
-              sm="6">
-                <v-text-field
-                  label="출발시간 (ex.8:00)*"
-                  v-model="input_start_time"
-                  :rules="[() => !!input_start_time || '시간을 입력하세요.']"
-                ></v-text-field>
-              </v-col>
 
 
-              <v-col 
+
+          <v-col 
               cols="12"
               sm="6"
               >
                 <v-text-field
+                  ref="input_start_point"
                   label="출발지*"
                   v-model="input_start_point"
                   :rules="[() => !!input_start_point || '출발지를 입력하세요.']"
+                  required
+                  placeholder="ex) 주안역"
                 ></v-text-field>
               </v-col>
+
+
+
+
+
+
+
+            <v-col 
+              cols="12"
+              sm="6">
+                <v-text-field
+                  ref="input_start_time"
+                  label="출발시간*"
+                  v-model="input_start_time"
+                  :rules="[() => !!input_start_time || '출발시간을 입력하세요.']"
+                  required
+                  placeholder="ex) 8:00"
+                ></v-text-field>
+              </v-col>
+
+
+
+              
 
 
 
@@ -202,19 +182,28 @@ sm="6">
               cols="12"
               sm="6">
                 <v-text-field
+                  ref="input_destination"
                   label="목적지*"
                   v-model="input_destination"
                   :rules="[() => !!input_destination || '목적지를 입력하세요.']"
+                  required
+                  placeholder="ex) 8:00"
                 ></v-text-field>
               </v-col>
+
+
+
 
               <v-col 
               cols="12"
               sm="6">
                 <v-text-field
-                  label="목적지 도착 시간 (ex.08:50)*"
+                  ref="input_destination_time"
+                  label="목적지 도착 시간*"
                   v-model="input_destination_time"
                   :rules="[() => !!input_destination_time || '목적지 도착 시간을 입력 하세요.']"
+                  required
+                  placeholder="ex) 8:50"
                 ></v-text-field>
               </v-col>
 
@@ -223,9 +212,12 @@ sm="6">
               cols="12"
               sm="6">
                 <v-text-field
+                  ref="input_car_number"
                   label="차량 번호*"
                   v-model="input_car_number"
-                  :rules="[() => !!input_car_number | '차량번호를 입력 하세요.']"
+                  :rules="[() => !!input_car_number || '차량번호를 입력 하세요.']"
+                  required
+                  placeholder="xx더 xxxx"
                 ></v-text-field>
               </v-col>
 
@@ -234,44 +226,65 @@ sm="6">
               cols="12"
               sm="6">
                 <v-text-field
+                  ref="input_car_type"
                   label="차 종류*"
                   v-model="input_car_type"
                   :rules="[() => !!input_car_type || '차종을 입력하세요.']"
+                  required
+                  placeholder="ex) 8:00"
                 ></v-text-field>
               </v-col>
-
-             
             </v-row>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
+
+
+        <v-divider class="mt-5"></v-divider>
         <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Close
+          <v-btn 
+          flat
+          @click="dialog = false"
+          text
+          >Cancel
           </v-btn>
 
-          <v-btn
-            color="blue darken-1"
-            text
-            
-            @click="[insertNew(),dialog=false]"
-          >
-            Save
-          </v-btn>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+
+          <v-slide-x-reverse-transition>
+            <v-tooltip
+              v-if="formHasErrors"
+              bottom
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  @click="resetForm"
+                  v-on="on"
+                  text
+                >
+                refresh
+                </v-btn>
+              </template>
+              <span>내용 지우기</span>
+            </v-tooltip>
+          </v-slide-x-reverse-transition>
+
+           <v-spacer></v-spacer>
+
+
+          <v-btn color="primary" flat @click="[submit(),insertNew()]" text>Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 </v-col>
 
-<v-col
-sm="6"
-md="1">
-    <p>  </p>
+  <v-col
+    sm="6"
+    md="1">
       <v-btn
        color="primary">
 
@@ -316,20 +329,25 @@ export default {
             date: [], // 기본 날짜
             dialog: false,
 
-            input_name:'', // 입력 받은 데이터
-            input_gender:'', // 입력 받은 데이터
-            input_seat:'',
-            input_period:'',
-            input_day:'',
-            input_start_time:'',
-            input_start_point:'',
-            input_destination:'',
-            input_destination_time:'',
-            input_car_number:'',
-            input_car_type:'',
+  
 
-            formHasErrors: false, // 유효성 검사
             errorMessages: '',
+
+            input_name: null,
+            input_gender: null,
+            input_seat: null,
+            input_period: null,
+            input_day: null,
+            input_start_point:null,
+            input_start_time: null,
+            input_destination:null,
+            input_destination_time:null,
+            input_car_number:null,
+            input_car_type:null,
+
+            formHasErrors: false,
+
+
 
 
       carpool_list: [
@@ -419,12 +437,60 @@ export default {
       }
     },
 
-   methods: {
-    insertNew() { // 새글쓰기 구현
+    computed: {
+      form () {
+        return {
+          input_name: this.input_name,
+          input_gender: this.input_gender,
+          input_seat: this.input_seat,
+          input_period: this.input_period,
+          input_day: this.input_day,
+          input_start_point: this.input_start_point,
+          input_start_time: this.input_start_time,
+          input_destination:this.input_destination,
+          input_destination_time:this.input_destination_time,
+          input_car_number:this.input_car_number,
+          input_car_type:this.input_car_type
+        }
+      }
+    },
+
+    watch: {
+      name () {
+        this.errorMessages = ''
+      }
+    },
+
+    methods: {
+      
+      resetForm () {
+        this.errorMessages = []
+        this.formHasErrors = false
+
+        Object.keys(this.form).forEach(f => {
+          this.$refs[f].reset()
+        })
+      },
+      submit () {
+        this.formHasErrors = false
+
+        Object.keys(this.form).forEach(f => {
+          if (!this.form[f]) this.formHasErrors = true
+
+          this.$refs[f].validate(true)
+        })
+
+       if(this.formHasErrors==false){
+          this.dialog=false
+        }
+
+      },
+
+      insertNew() { // 카풀 등록 함수
 
     
     
-  this.carpool_list.push({
+        this.carpool_list.push({
         "name":this.input_name,
         "gender":this.input_gender, 
         "seat":this.input_seat,
@@ -436,18 +502,7 @@ export default {
         "estimated_destination_time":this.input_destination_time, 
         "extra_time":"", 
         "extra_distance":"" })
-    },
-    chekedform(){
-     this.formHasErrors = false
-
-        Object.keys(this.carpool_list).forEach(f => {
-          if (!this.carpool_list[f]) this.formHasErrors = true
-
-          this.$refs[f].validate(true) 
-        })
+       },
     }
-
-    
-   }
-}
+  }
 </script>
